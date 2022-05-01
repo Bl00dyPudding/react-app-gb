b509fcf002e9ed646ca8a099dec4138666ae0c12
Bloody Pudding <bloodypudding@gmail.com>
2022-05-01 23:12:08 +0300

    Messages component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from "./Message";
+
+const messages = ['❤️ Vue ❤️', '💩 React 💩'];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{messages.map(message => (
+				<Message message={message}/>
+			))}
+		</div>
+	);
 }
 
 export default App;
