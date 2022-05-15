5530ff0e80878d04a5c73e093e6b94560b31b868
Bloody Pudding <bloodypudding@gmail.com>
2022-05-15 23:47:24 +0300

    now you can send message and bot will answer you

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import './App.css';
-import Message from "./Message";
-
-const messages = ['❤️ Vue ❤️', '💩 React 💩'];
+import {useEffect, useState} from "react";
 
 function App() {
+	const [messages, setMessages] = useState([])
+
 	return (
 		<div className="App">
-			{messages.map(message => (
-				<Message message={message}/>
+			<Input setMessages={setMessages} messages={messages}/>
+
+			{messages.map((message, i) => (
+				<div key={i}>
+					<b>{message.author}</b>: <span>{message.value}</span>
+				</div>
 			))}
+		</div>
+	);
+}
+
+export function Input({setMessages, messages}) {
+	const [value, setValue] = useState('');
+
+	const createAnswer = t => {
+		setTimeout(() => {
+			setMessages((m) => [...m, {author: 'bot', value: 'React'}])
+		}, t);
+	};
+
+	useEffect(() => {
+		if (messages.length && messages[messages.length - 1].author !== 'bot') {
+			createAnswer(500)
+		}
+	}, [messages]);
+
+	const updateMessages = () => {
+		setMessages((m) => [...m, {author: 'user', value}]);
+		setValue('');
+	}
+
+	return (
+		<div className="Input">
+			<input
+				type="text"
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+			<button onClick={updateMessages}>Send message!</button>
 		</div>
 	);
 }
--- src/Message.js
+++
@@ -1,9 +0,0 @@
-function Message({message}) {
-	return (
-		<div>
-			{message}
-		</div>
-	);
-}
-
-export default Message;
